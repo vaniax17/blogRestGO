b052462393e48ed9b6f78ab25242e9b731b3b6c3
vaniax17 <>
2025-08-14 19:47:19 +0300

    slug in the all comments

--- src/models/comments.go
+++ src/models/comments.go
@@ -4,7 +4,8 @@ type Comment struct {
 	Id             int    `gorm:"primary_key"`
 	Content        string `gorm:"not null"`
 	AuthorUsername string `gorm:"not null"`
-	PostSlug       string `gorm:"not null"`
+	PostSlug       string `gorm:"not null;index;"`
+	Slug           string `gorm:"not null;index;"`
 	CreatedAt      uint64 `gorm:"autoCreateTime"`
 	UpdatedAt      uint64 `gorm:"autoUpdateTime"`
 }
