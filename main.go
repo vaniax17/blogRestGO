b21018d6076a128ce46d974263706c21746a7238
vaniax17 <>
2025-08-14 15:16:11 +0300

    changes with dot env files loads

--- main.go
+++ main.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"blogRest/src/api"
 	"blogRest/src/db"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic("Error loading .env file")
+	}
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
 	db.Init()
+	api.RoutesInit(e)
 
 	if err := e.Start(":8080"); err != nil {
 		e.Logger.Fatal(err)
