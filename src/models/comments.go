e01e5c805b78f87bd14acc7758c0a90c0746940a
vaniax17 <>
2025-08-14 14:02:18 +0300

    fixed bug with int == string

--- src/models/comments.go
+++ src/models/comments.go
@@ -3,7 +3,7 @@ package models
 type Comment struct {
 	Id             int    `gorm:"primary_key"`
 	Content        string `gorm:"not null"`
-	AuthorUsername int    `gorm:"not null"`
+	AuthorUsername string `gorm:"not null"`
 	PostSlug       string `gorm:"not null"`
 	CreatedAt      uint64 `gorm:"autoCreateTime"`
 	UpdatedAt      uint64 `gorm:"autoUpdateTime"`
