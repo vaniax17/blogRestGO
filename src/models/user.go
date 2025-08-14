f881e7651667fa4354dc95228ac62b5486760a23
vaniax17 <>
2025-08-14 13:51:19 +0300

    user model for db

---
+++ src/models/user.go
@@ -0,0 +1,10 @@
+package models
+
+type User struct {
+	Id        int    `gorm:"primary_key"`
+	Username  string `gorm:"unique;not null;index;"`
+	Password  string `gorm:"not null;"`
+	Email     string `gorm:"unique;not null;index;"`
+	CreatedAt uint64 `gorm:"autoCreateTime"`
+	UpdatedAt uint64 `gorm:"autoUpdateTime"`
+}
