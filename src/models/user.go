5963fbe5b27e31f2abd8befe5363b5571368b137
vaniax17 <>
2025-08-14 15:23:59 +0300

    update with foreignkey

--- src/models/user.go
+++ src/models/user.go
@@ -5,6 +5,7 @@ type User struct {
 	Username  string `gorm:"unique;not null;index;"`
 	Password  string `gorm:"not null;"`
 	Email     string `gorm:"unique;not null;index;"`
+	Posts     []Post `gorm:"foreignkey:AuthorUsername"`
 	CreatedAt uint64 `gorm:"autoCreateTime"`
 	UpdatedAt uint64 `gorm:"autoUpdateTime"`
 }
