42ee18afa261eee024aa46ab03a5d9a9d8008641
vaniax17 <>
2025-08-14 13:38:41 +0300

    default main for echo backend

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
 package blogRest
+
+import (
+	"blogRest/src/db"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	if err := e.Start(":8080"); err != nil {
+		e.Logger.Fatal(err)
+		db.Close()
+	}
+
+}
