eb7104fef09d8b1bd96870fef950b2a6dac729cf
vaniax17 <>
2025-08-14 16:09:53 +0300

    removed duplicated code and set this code to utils

---
+++ src/core/utils.go
@@ -0,0 +1,24 @@
+package core
+
+import (
+	"blogRest/src/db"
+	"blogRest/src/models"
+
+	"github.com/labstack/echo/v4"
+)
+
+func SetAndGenerateToken(username string, c echo.Context) {
+
+	token, err := GenerateToken(username)
+
+	if err != nil {
+		return
+	}
+
+	c.Response().Header().Set("Authorization", "Bearer "+token)
+
+}
+
+func GetWHereUser(username string, user *models.User) {
+	db.DB.Where("username = ?", username).First(user)
+}
