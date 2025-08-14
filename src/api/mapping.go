31d0ca401f1001989fec5bf352d7afbaab73e3a2
vaniax17 <>
2025-08-14 15:49:43 +0300

    mapper for get user posts by username

--- src/api/mapping.go
+++ src/api/mapping.go
@@ -10,5 +10,6 @@ func RoutesInit(e *echo.Echo) {
 	e.GET("/user/login", user.Login)
 	e.POST("/user/register", user.Create)
 	e.GET("/user/all/usernames", user.GetAll)
+	e.GET("/user/get/posts", user.GetPosts)
 
 }
