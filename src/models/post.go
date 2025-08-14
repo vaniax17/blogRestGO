1bbf83edb3d70280983db0f4c61523164a53a5f9
vaniax17 <>
2025-08-14 13:53:40 +0300

    post model for db

---
+++ src/models/post.go
@@ -0,0 +1,11 @@
+package models
+
+type Post struct {
+	Id        int    `gorm:"primary_key"`
+	Title     string `gorm:"not null"`
+	Content   string `gorm:"not null"`
+	Slug      string `gorm:"not null;index;unique"`
+	AuthorId  int    `gorm:"not null"`
+	CreatedAt uint64 `gorm:"autoCreateTime"`
+	UpdatedAt uint64 `gorm:"autoUpdateTime"`
+}
